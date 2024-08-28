70ffd5f11f7d7f6bb3d176574f3ad965b95ad247
Kevin Carrasco <kepler401@gmail.com>
2024-08-28 16:27:41 -0600

    Actualización y creacion de héroes implementado

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-import { Publisher } from '../../interfaces/hero.interface';
+import { Hero, Publisher } from '../../interfaces/hero.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'heroes-new-page',
@@ -22,10 +23,21 @@ export class NewPageComponent {
     { id:'Marvel Comics', desc:'Marvel - Comics' },
   ];
 
+  constructor(private heroService:HeroesService){}
+
+  get currentHero(): Hero{
+    const hero = this.heroForm.value as Hero;
+    return hero;
+
+  }
   onSubmit(): void{
-    console.log({
-      formIsValid: this.heroForm.valid,
-      value: this.heroForm.value
-    })
+    if(this.heroForm.invalid) return;
+    if(this.currentHero.id){
+      this.heroService.updateHero(this.currentHero)
+        .subscribe( hero => {
+          // Mostrar snackbar
+        });
+      return;
+    }
   }
 }
